c2b1b40c38a277abfcfb10fdcdb8e90f329aa983
BrisetRoque <166642138+BrisetRoque@users.noreply.github.com>
2025-09-02 13:53:56 -0600

    Terminamos la funcion inverso

--- CalculosTDD/src/Calculos.java
+++ CalculosTDD/src/Calculos.java
@@ -44,7 +44,16 @@ public class Calculos {
      * @return texto invertida
      */
     String invertir(String texto) {
-        throw new UnsupportedOperationException("En construcción.");
+        para la funcion inverso: 
+        String[] palabras = texto.split(" ");
+        StringBuilder resultado = new StringBuilder();
+
+        for (String palabra : palabras) {
+            StringBuilder palabraInvertida = new StringBuilder(palabra);
+            resultado.append(palabraInvertida.reverse().toString()).append(" ");
+        }
+
+        return resultado.toString().trim();
     }
 
 }
